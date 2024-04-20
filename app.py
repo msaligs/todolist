d52e11c1e0eef32b7f784afa00cfe05fd94003d4
msaligs <msaligs@email.com>
2024-04-21 05:17:29 +0530

    user registration and login done

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, redirect, render_template,flash
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from model import *
 from resources import *
 from flask_cors import CORS 
 import requests
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///task.sqlite3"
@@ -15,55 +16,146 @@ app.app_context().push()
 api.init_app(app)
 CORS(app)
 
-@app.route('/', methods=['GET','POST'])
-def ToDoList():
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+@app.route('/')
+def home():
+    if current_user.is_authenticated:
+        return redirect('/task')
+    return render_template('index.html')
+
+
+@app.route('/user_login' , methods = ['GET','POST'])
+def user_login():
+    if current_user.is_authenticated:
+        return redirect('/task')
     if request.method == 'GET':
-        # tasks = Todo.query.order_by(Todo.date_created).all()
-        tasks = TaskList.query.order_by(TaskList.date_created.desc()).all()
-        # tasks = TaskList.query.all()
-        return render_template('index.html', tasks = tasks)
+        return render_template('login.html')
     
-    elif request.method =='POST':
-        task_content = request.form.get("content")
-        
-        task = TaskList.query.filter_by(content = task_content).first()
-        if task is None:
-            new_task = TaskList(content = task_content)
-            db.session.add(new_task)
-            try:
-                flash("New task created!","success")
-                db.session.commit()
-                return redirect('/')
-            except:
-                flash("something went wrong while creating task! Try again.","danger")
-                return redirect('/')
+    elif request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email = email).first()
+
+        if user is not None and check_password_hash(user.password,password):
+            login_user(user)
+            flash("Login successful!","success")
+            return redirect('/task')
         else:
-            flash("Task already exist!","warning")
+            flash("Invalid email or password!","danger")
             return redirect('/')
 
 
-@app.route('/delete/<int:id>',methods = ['GET','POST'])
-def delete(id):
-    # get the url on which application is running
-    # url = request.host
-    response = requests.delete(f"http://{request.host}/api/delete_task/{id}")
-    if response.status_code == 204:
-        flash("Task deleted successfully!","success")
-        return redirect('/')
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
+
+
+@app.route('/task', methods=['GET','POST'])
+@login_required
+def task():
+    if request.method == 'GET':
+        # tasks = Todo.query.order_by(Todo.date_created).all()
+        id = current_user.id
+        tasks = Task.query.filter_by(user_id = id).order_by(Task.date_created.desc()).all()
+        # tasks = Task.query.order_by(Task.date_created.desc()).all()
+        # tasks = Task.query.all()
+        return render_template('task.html', tasks = tasks)
+        return render_template('login.html')
     
-    elif response.status_code == 404:
-        flash("No Task with the given id!","warning")
-        return redirect('/')
-    else:
-        flash(f"An error occurred: {response.status_code}","danger")
-        return redirect('/')
+    elif request.method =='POST':
+        task_content = request.form.get("task")
+        
+        task = Task.query.filter_by(content = task_content).first()
+        if task is None:
+            new_task = Task(content = task_content,user_id = current_user.id)
+            db.session.add(new_task)
+            try:
+                db.session.commit()
+                flash("New task created!","success")
+                return redirect('/task')
+            except Exception as e:
+                flash("something went wrong while creating task! Try again.","danger")
+                print(datetime.utcnow())
+                print(e)
+                return redirect('/task')
+        else:
+            flash("Task already exist!","warning")
+            return redirect('/task')
+
+
+@app.route('/register',methods = ['GET','POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email = email).first()
+        if user is None:
+            new_user = User(username = username,email = email,password = generate_password_hash(password))
+
+            db.session.add(new_user)
+            try:
+                db.session.commit()
+                flash("New user created successfully!","success")
+                return redirect('/user_login')
+            except:
+                flash("Something went wrong while creating user! Try again.","danger")
+                return redirect('/register')
+        else:
+            flash("email already exist!","warning")
+            return redirect('/register')
+
+@app.route('/complete/<int:id>')
+@login_required
+def complete(id):
+    task = Task.query.get_or_404(id)
+    task.completed = not task.completed
+    task.date_completed = datetime.now().strftime("%Y-%m-%d %H:%M")
+    try:
+        db.session.commit()
+        flash("Task completed successfully!","success")
+        return redirect('/task')
+    except:
+        flash("Something went wrong while completing the task!","danger")
+        return redirect('/task')
+
+
+# @app.route('/delete/<int:id>',methods = ['GET','POST'])
+# def delete(id):
+#     # get the url on which application is running
+#     # url = request.host
+#     response = requests.delete(f"http://{request.host}/api/delete_task/{id}")
+#     if response.status_code == 204:
+#         flash("Task deleted successfully!","success")
+#         return redirect('/')
+    
+#     elif response.status_code == 404:
+#         flash("No Task with the given id!","warning")
+#         return redirect('/')
+#     else:
+#         flash(f"An error occurred: {response.status_code}","danger")
+#         return redirect('/')
     
 
 
 
 @app.route('/update/<int:id>',methods = ['GET','POST'])
+@login_required
 def  update(id):
-    task = TaskList.query.get_or_404(id)
+    task = Task.query.get_or_404(id)
     
     if request.method == 'POST':
         task.content = request.form.get('content')
@@ -81,4 +173,4 @@ def  update(id):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- model.py
+++ model.py
@@ -1,11 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from datetime import datetime
 
 db =SQLAlchemy()
 
-class TaskList(db.Model):
-    id = db.Column(db.Integer(),primary_key = True, nullable = False)
+class Task(db.Model):
+    id = db.Column(db.Integer(),primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
     content = db.Column(db.String(200),nullable = False,unique = True)
     date_created = db.Column(db.String(20) , default =lambda : datetime.now().strftime("%Y-%m-%d %H:%M") )
+    # date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_completed = db.Column(db.String(20))
+    completed = db.Column(db.Boolean, default = False)
     def __repr__(self):
         return '<Task %r>' % self.id
+    
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True,unique=True, nullable = False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable = False)
+    password = db.Column(db.String(128), nullable = False)
+
+    tasks = db.relationship('Task', backref='user', lazy='dynamic')
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
